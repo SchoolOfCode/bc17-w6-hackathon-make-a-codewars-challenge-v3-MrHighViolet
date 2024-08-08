bf5b9e595c9b185472a2f9fcafac305f9471cd60
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-08 20:05:33 +0100

    3 tests done more ideas

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-/*A few examples:
-     `transformLength("table")` should return -1 as the length of "table" is not even
-     `transformLength("wizard")` should return 1 as the length of "wizard" is even
-  
+/*
  Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
  For every 10 of armour player has, the damage is reduced by 5%.
+ Ideas: 
+ pass in an object(player) which can have armour boolean, if has armour do something
+ it could have heal spell key, if "Auto" , casts if health hits 0 and boosts back to 50
+
  */
 
 export function combat(health, damage, armour) {
