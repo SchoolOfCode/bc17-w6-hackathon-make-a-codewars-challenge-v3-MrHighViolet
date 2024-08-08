a9f0d29c0c9c2a392331799d93cee2eeff8ced09
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-08 21:33:46 +0100

    win true false

--- main.js
+++ main.js
@@ -8,18 +8,25 @@ Carl, along with his cat, Princess Donut, is an ordinary guy who suddenly finds 
  could have training key - hours spent (int) - if this over 24 then the Cockroach skill is acquired (means you survive a fatal hit (health back to 5))
  if training over 12h and have armour, damage reduced by 75%
  newhealth rounded to nearest int
+ in the dungeon, even days are lucky, odd are unlucky.
+ if he lost & it's am unlucky day, damage is increased by 30%
+ if he won and its a lucky day, damage is decreased by 30%^
+ win: true or false
  */
 
 export function combat(carl, damage) {
     // Temporarily added the below to check if tests pass
     if (carl.armour === true) {
         if (carl.training > 12) {
-            damage -= damage / 1.33;
+            damage *= 0.25;
         } else {
-        damage -= damage / 2;
-        }
-    }
-   
+        damage *= 0.5;
+        };
+    };
+    
+    // if (carl.constitution < 30 && carl.health <= 20) {
+    //     damage *= 1.3;
+    // }    
     const newHealth = carl.health - damage;
     
     if (newHealth < 0) {
