4cf16fe4f27db550392a0a667966d0228e54ff05
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-09 10:23:12 +0100

    wrote pres

--- PROJECT_PRESENTATION.md
+++ PROJECT_PRESENTATION.md
@@ -1,1 +1,53 @@
-Put your plan, story, and any supporting material here, you could use the help of a readme markdown generator such as [readme.so](https://readme.so/)
+# Dungeon Crawler Carl Kata 
+## By Jonathan Hill (MrHighViolet)
+
+Here is a Kata based on the popular Dungeon Crawler Carl series of LitRPG books by Matt Dinniman.
+
+I took inspiration from this CodeWars kata (https://www.codewars.com/kata/586c1cf4b98de0399300001d).
+
+I am always impressed by how games/authors work out how much damage is taken by a player/character using a complicated algorithm. I tried to recreate this here.
+
+## Description
+
+Carl, along with his cat, Princess Donut, is an ordinary guy who suddenly finds himself thrust into a deadly, game-like dungeon after Earth is destroyed by aliens. He is surrounded by ogres, goblins and even occasionally level bosses. He needs your help in keeping track of his health after each round of combat.
+Create a combat function that takes the object(carl) and the amount of damage received, and returns the player's new health(rounded to the nearest integer). Health can't be less than 0.
+
+The carl object includes the following keys: health(int), armour(boolean), training(int representing hours spent training)
+
+Rules of the dungeon:
+
+- if a player has armour, damage is reduced by 50%
+- if a player has armour and they've spent over 12 hours training, damage is reduced by 75%
+- if a player has spent over 24 hours training they develop an auto Cockroach skill (Description: you survive a fatal hit with your health returning to 5)
+
+## Ideal world
+
+I would like to expand this idea so that multiple rounds of combat are required. This would need a combat counter and for carl.health to be updated after each round.
+
+## Solution
+
+<details>
+    <summary>Click for a solution</summary>
+
+```js
+export function combat(carl, damage) {
+    if (carl.armour === true) { 
+        if (carl.training > 12) {
+            damage *= 0.25; // Reduced by 75%
+        } else {
+        damage *= 0.5; // Reduced by 50%
+        };
+    };
+    
+    const newHealth = carl.health - damage;
+    
+    if (newHealth < 0) { // Fatal hit
+        if (carl.training > 24) { // Cockroach skill
+            return 5;
+        }
+        return 0;
+    }
+    return Math.round(newHealth); 
+};
+```
+</details>
--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 /*
-Carl, along with his cat, Princess Donut, is an ordinary guy who suddenly finds himself thrust into a deadly, game-like dungeon after Earth is destroyed by aliens. He is surrounded by ogres, goblins and even level bosses. He needs your help in keeping track of his health after each round of combat.
- Create a combat function that takes the object(carl) and the amount of damage received, and returns the player's new health. Health can't be less than 0.
- For every 10 of armour player has, the damage is reduced by 5%.
- Ideas: 
- pass in an object(player) which has health key (int), can have armour key (boolean), if has armour: reduce damage by 50%
- it has 
- could have training key - hours spent (int) - if this over 24 then the Cockroach skill is acquired (means you survive a fatal hit (health back to 5))
- if training over 12h and have armour, damage reduced by 75%
- newhealth rounded to nearest int
- in the dungeon, even days are lucky, odd are unlucky.
- if he lost & it's am unlucky day, damage is increased by 30%
- if he won and its a lucky day, damage is decreased by 30%^
- win: true or false
+Carl, along with his cat, Princess Donut, is an ordinary guy who suddenly finds himself thrust into a deadly, game-like dungeon after Earth is destroyed by aliens. He is surrounded by ogres, goblins and even occasionally level bosses. He needs your help in keeping track of his health after each round of combat.
+Create a combat function that takes the object(carl) and the amount of damage received, and returns the player's new health(rounded to the nearest integer). Health can't be less than 0.
+
+The carl object includes the following keys: health(int), armour(boolean), training(int representing hours spent training)
+
+Rules of the dungeon:
+- if a player has armour, damage is reduced by 50%
+- if a player has armour and they've spent over 12 hours training, damage is reduced by 75%
+- if a player has spent over 24 hours training they develop an auto Cockroach skill (Description: you survive a fatal hit with your health returning to 5)
+
+After you've completed the function, test it by running 'npm install' and then 'npm run test'.
  */
 
 export function combat(carl, damage) {
@@ -24,9 +22,6 @@ export function combat(carl, damage) {
         };
     };
     
-    // if (carl.constitution < 30 && carl.health <= 20) {
-    //     damage *= 1.3;
-    // }    
     const newHealth = carl.health - damage;
     
     if (newHealth < 0) {
